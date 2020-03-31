56d85d207fdb6d8f518a49c29067ce881504507e
Joseph Kim <thejykim1@gmail.com>
2020-03-31 19:36:39 -0400

    Confirmed ability to play multiple notes at the same time

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,5 +1,9 @@
 var synth = new Tone.Synth().toMaster();
+var synth2 = new Tone.Synth().toMaster();
+var synth3 = new Tone.Synth().toMaster();
 
 function playNote() {
-    synth.triggerAttackRelease('C4', '8n');
+    synth.triggerAttackRelease('C4', '4n');
+    synth2.triggerAttackRelease('A4', '4n');
+    synth3.triggerAttackRelease('E4', '4n');
 }
