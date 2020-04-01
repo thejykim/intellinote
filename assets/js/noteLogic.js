62fc918bb9f1f987ac1c59f0dbd36aa487df2b3a
BryantH24 <gbhou5@gmail.com>
2020-03-31 20:31:52 -0400

    linked note length with buttons, lengths change on click

--- assets/js/noteLogic.js
+++ assets/js/noteLogic.js
@@ -1,5 +1,29 @@
 // assignNote() saves the currently selected note
+let currentNote ="";
 function assignNote() {
      let form = document.getElementById("form");
-     var currentNote = form.elements["note"].value;
+     currentNote = form.elements["note"].value;
+}
+//default note length (change to match documentation)
+noteLen = 1;
+// parseNoteLen() finds location based on id (string), sets noteLen to correct length for
+// onToggleNote()
+function parseNoteLen() {
+  let lengths = ["eighthNote", "quarterNote", "halfNote", "wholeNote"];
+  var lenArrLoc = lengths.indexOf(currentNote);
+  // add correct lengths based on api documentation
+  switch(lenArrLoc){
+    case 0:
+       noteLen = 8;
+       break;
+    case 1:
+       noteLen = 16;
+       break;
+    case 2:
+       noteLen = 32;
+       break;
+    case 3:
+       noteLen = 64;
+       break;
+  }
 }
