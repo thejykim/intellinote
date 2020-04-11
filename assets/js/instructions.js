08d5431f68368cb83f4c1e716db9a59a579a03a8
Joseph Kim <thejykim1@gmail.com>
2020-04-11 14:20:52 -0400

    fixed info modal

--- assets/js/instructions.js
+++ assets/js/instructions.js
@@ -3,18 +3,18 @@ function setInfoModalJS() {
       event.preventDefault();
       var infoModal = document.querySelector('#infoModal');
       var html = document.querySelector('html');
-      exportModal.classList.add('is-active');
+      infoModal.classList.add('is-active');
       html.classList.add('is-clipped');
 
      infoModal.querySelector('#background-info').addEventListener('click', function (e) {
           e.preventDefault();
-         exportModal.classList.remove('is-active');
+         infoModal.classList.remove('is-active');
           html.classList.remove('is-clipped');
       });
 
      infoModal.querySelector('#close-info').addEventListener('click', function (e) {
           e.preventDefault();
-         exportModal.classList.remove('is-active');
+         infoModal.classList.remove('is-active');
           html.classList.remove('is-clipped');
       });
   });
