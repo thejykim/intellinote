7f9cb8bdba94c765bbab1e1bc68ea1a09a9cf446
BryantH24 <gbhou5@gmail.com>
2020-05-17 20:35:18 -0400

    fixing the js

--- assets/js/parseTextFromServer.js
+++ assets/js/parseTextFromServer.js
@@ -1,4 +1,4 @@
-// const serverData = String.raw`Don't Stop The Party|secs@gmail.com|04-20-2020|05-10-2020|00202020?YeetYeet|secs@gmail.com|04-20-2020|05-10-2020|00202020`;
+const serverData = String.raw`Don't Stop The Party|secs@gmail.com|04-20-2020|05-10-2020|00202020?YeetYeet|secs@gmail.com|04-20-2020|05-10-2020|00202020`;
 const fieldParse = "|";
 const songParse = "?";
 const dateSep = "-"
@@ -30,10 +30,39 @@ function parseServerData(serverData){
 
 // Send all songs to database
 // TODO
-function sendNewSong(newSong) {
+// function sendNewSong(newSong) {
+//     var data = new XMLHttpRequest();
+//     var params = `newString=${newSong}`;
+// 	data.onload = function() {
+// 	    if (data.status == 200 && data.readyState == 4) {
+//             console.log(data.responseText);
+//             // parseServerData(data.responseText);
+//         }
+//         console.log("Running");
+//     };
+//     data.open("POST", "assets/php/create-song.php", true);
+//     data.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//     data.send(params); //parems
+// }
+// sendNewSong(serverData);
+
+// Format fields into database-readable String
+// TODO
+function createNewSong(){
+    var curDate = new Date();
+    let newSong = "";
+    exportSong();
+
+    userID = document.getElementById('emailID').value;
+    title = document.getElementById('title').value;
+    month = (curDate.getMonth()+1).toString().concat(dateSep);
+    day = curDate.getDate().toString().concat(dateSep);
+    year = curDate.getFullYear().toString();
+    date = "";
+    date = date.concat(month, day, year);
     var data = new XMLHttpRequest();
-    var params = `newString=${newSong}`;
-	data.onload = function() {
+    var params = `id=${userID}, title=${title}, dateCreated=${dateCreated}, dateModified=${dateCreated}, songData=${textbox.value}`;
+	  data.onload = function() {
 	    if (data.status == 200 && data.readyState == 4) {
             console.log(data.responseText);
             // parseServerData(data.responseText);
@@ -43,40 +72,11 @@ function sendNewSong(newSong) {
     data.open("POST", "assets/php/create-song.php", true);
     data.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     data.send(params); //parems
-}
+    // // console.log(day);
+    // newSong = newSong.concat(email, title, month, day, year, month, day, year, textbox.value, songParse);
+    // parseServerData(newSong);
+    // sendNewSong(newSong);
 
-// Format fields into database-readable String
-// TODO
-function createNewSong(){
-    var curDate = new Date();
-    let newSong = "";
-    exportSong();
-
-    email = document.getElementById('emailID').value.concat(fieldParse);
-    title = document.getElementById('title').value.concat(fieldParse);
-    month = (curDate.getMonth()+1).toString().concat(dateSep);
-    day = curDate.getDate().toString().concat(dateSep);
-    year = curDate.getFullYear().toString().concat(fieldParse);
-
-    // console.log(day);
-    newSong = newSong.concat(email, title, month, day, year, month, day, year, textbox.value, songParse);
-    parseServerData(newSong);
-    sendNewSong(newSong);
-
-
-    // turn form vals into formatted string
-    // serverEachSong = serverData.split("?");
-    // for (let i = 0; i < serverEachSong.length; i++) {
-    //     let songStr = serverEachSong[i].split("|");
-    //     var songObj = {
-    //         title : songStr[0],
-    //         id : songStr[1],
-    //         dateCreated :songStr[2],
-    //         dateModified : songStr[3],
-    //         songData : songStr[4]
-    //     };
-    //     serverObjects.push(songObj);
-    // }
 
 }
 
