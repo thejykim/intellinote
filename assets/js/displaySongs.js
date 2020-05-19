d24b694784ba29da753e2a3e49e79a376db90509
Joseph Kim <thejykim1@gmail.com>
2020-05-19 19:36:31 +0900

    Let's try this

--- assets/js/displaySongs.js
+++ assets/js/displaySongs.js
@@ -6,100 +6,97 @@ let recentThreshold = 2592000000;
 // purely for functionality testing
 let userID = "hello@hello.com";
 
-function asyncGetSongs(userID) {
-    return new Promise(function(resolve, reject){
-        getSongs(userID);
+function displaySongs() {
+    var data = new XMLHttpRequest();
+	data.onload = function() {
+	    if (data.status == 200 && data.readyState == 4) {
+            parseServerData(data.responseText);
+            if (serverObjects.length == 0) {
+                // show zero songs
+                return;
+            }
 
-        resolve(serverObjects);
-    });
-}
+            let recentSongs = [];
+            let currentDate = Date.now();
 
-async function displaySongs() {
-    console.log("displaySongs() ran");
-    let something = await asyncGetSongs(userID);
-    console.log("got songs!");
+            allSongsDiv.innerHTML = `
+            <div class="columns" style="padding-bottom: 1.5rem">
+                <div class="column is-6 is-offset-2">
+                    <h4 class="title is-4 poppins">All songs</h4>
+                    <h6 class="subtitle is-6 karla">${serverObjects.length} songs</h6>
+                </div>
+            </div>
+            `;
 
-    if (serverObjects.length == 0) {
-        // show zero songs
-        return;
-    }
+            // copy eligible songs into recent songs array
+            for (let i = 0; i < serverObjects.length; i++) {
+                console.log("serverObjects for loop ran");
+                let songDate = Date.parse(serverObjects[i]);
 
-    let recentSongs = [];
-    let currentDate = Date.now();
+                if ((currentDate - songDate) < recentThreshold) {
+                    recentSongs.push(serverObjects[i]);
+                }
 
-    allSongsDiv.innerHTML = `
-    <div class="columns" style="padding-bottom: 1.5rem">
-        <div class="column is-6 is-offset-2">
-            <h4 class="title is-4 poppins">All songs</h4>
-            <h6 class="subtitle is-6 karla">${serverObjects.length} songs</h6>
-        </div>
-    </div>
-    `;
+                // briefly parse song data
+                let numRows = serverObjects[i].songData.split(sheetParse)[2];
 
-    // copy eligible songs into recent songs array
-    for (let i = 0; i < serverObjects.length; i++) {
-        console.log("serverObjects for loop ran");
-        let songDate = Date.parse(serverObjects[i]);
+                let songDiv = document.createElement('div');
+                songDiv.setAttribute("class", "columns");
+                songDiv.setAttribute("style", "padding-bottom: 0.5rem");
+                songDiv.innerHTML = `
+                <div class="column is-6 is-offset-3">
+                    <div class="card" style="border-radius: 20px; border: 1px solid lightgray">
+                        <div class="card-content">
+                            <div class="content">
+                                <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href="projects/ideadb" class="has-text-dark">${serverObjects[i].title}</a></h5>
+                                <span class="tag is-dark poppins">Created: ${serverObjects[i].dateCreated}</span>
+                                <span class="tag is-light poppins"><b>${numRows} rows</b></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
 
-        if ((currentDate - songDate) < recentThreshold) {
-            recentSongs.push(serverObjects[i]);
+                allSongsDiv.appendChild(songDiv);
+            }
+
+            // print recent songs
+            recentSongsDiv.innerHTML = `
+            <div class="columns" style="padding-bottom: 1.5rem">
+                <div class="column is-6 is-offset-2">
+                    <h4 class="title is-4 poppins">Recently modified</h4>
+                    <h6 class="subtitle is-6 karla">${recentSongs.length} songs</h6>
+                </div>
+            </div>
+            `;
+
+            for (let i = 0; i < recentSongs.length; i++) {
+                console.log("recentSongs for loop ran");
+                // briefly parse song data
+                let numRows = recentSongs[i].songData.split(sheetParse)[2];
+
+                let songDiv = document.createElement('div');
+                songDiv.setAttribute("class", "columns");
+                songDiv.setAttribute("style", "padding-bottom: 0.5rem");
+                songDiv.innerHTML = `
+                <div class="column is-6 is-offset-3">
+                    <div class="card" style="border-radius: 20px; border: 1px solid lightgray">
+                        <div class="card-content">
+                            <div class="content">
+                                <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href="#" class="has-text-dark">${recentSongs[i].title}</a></h5>
+                                <span class="tag gradient has-text-white poppins">Last updated: ${recentSongs[i].dateModified}</span>
+                                <span class="tag is-light poppins"><b>${numRows} rows</b></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+
+                recentSongsDiv.appendChild(songDiv);
+            }
         }
-
-        // briefly parse song data
-        let numRows = serverObjects[i].songData.split(sheetParse)[2];
-
-        let songDiv = document.createElement('div');
-        songDiv.setAttribute("class", "columns");
-        songDiv.setAttribute("style", "padding-bottom: 0.5rem");
-        songDiv.innerHTML = `
-        <div class="column is-6 is-offset-3">
-            <div class="card" style="border-radius: 20px; border: 1px solid lightgray">
-                <div class="card-content">
-                    <div class="content">
-                        <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href="projects/ideadb" class="has-text-dark">${serverObjects[i].title}</a></h5>
-                        <span class="tag is-dark poppins">Created: ${serverObjects[i].dateCreated}</span>
-                        <span class="tag is-light poppins"><b>${numRows} rows</b></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-
-        allSongsDiv.appendChild(songDiv);
-    }
-
-    // print recent songs
-    recentSongsDiv.innerHTML = `
-    <div class="columns" style="padding-bottom: 1.5rem">
-        <div class="column is-6 is-offset-2">
-            <h4 class="title is-4 poppins">Recently modified</h4>
-            <h6 class="subtitle is-6 karla">${recentSongs.length} songs</h6>
-        </div>
-    </div>
-    `;
-
-    for (let i = 0; i < recentSongs.length; i++) {
-        console.log("recentSongs for loop ran");
-        // briefly parse song data
-        let numRows = recentSongs[i].songData.split(sheetParse)[2];
-
-        let songDiv = document.createElement('div');
-        songDiv.setAttribute("class", "columns");
-        songDiv.setAttribute("style", "padding-bottom: 0.5rem");
-        songDiv.innerHTML = `
-        <div class="column is-6 is-offset-3">
-            <div class="card" style="border-radius: 20px; border: 1px solid lightgray">
-                <div class="card-content">
-                    <div class="content">
-                        <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href="#" class="has-text-dark">${recentSongs[i].title}</a></h5>
-                        <span class="tag gradient has-text-white poppins">Last updated: ${recentSongs[i].dateModified}</span>
-                        <span class="tag is-light poppins"><b>${numRows} rows</b></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-
-        recentSongsDiv.appendChild(songDiv);
-    }
+    };
+    data.open("POST", `assets/php/get-songs.php?userID=${userID}`, true);
+    data.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    data.send();
 }
