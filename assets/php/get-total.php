b42f3e2e06bacb8b0bfc925495dc53e21fb63154
Joseph Kim <thejykim1@gmail.com>
2020-05-23 10:17:26 +0900

    Total song counter

---
+++ assets/php/get-total.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+$id = $_GET["userID"];
+
+$servername = "167.88.161.21";
+$username = "thejykco_sheets_user";
+$password = file_get_contents(__DIR__ . "/protected/password.txt");
+$dbname = "thejykco_sheets";
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = "SELECT * FROM userSongs";
+
+$stmt = $connection->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result === false) {
+    die("Connection failed - try again in a few moments.");
+}
+
+echo mysqli_num_rows($result);
+
+$connection->close();
+?>
