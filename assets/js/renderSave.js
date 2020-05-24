4d98964adf838df2973134823ebfb9d3f7ad4964
Romi Asad <rmasad@unc.edu>
2020-05-24 16:54:38 -0400

    ??

--- assets/js/renderSave.js
+++ assets/js/renderSave.js
@@ -1,5 +1,6 @@
 function renderSave() {
+    console.log("Hello I am running");
     getSong();
     console.log(savedSong);
-    console.log("Hello I am running");
+    console.log("Hello I am running twice");
 }
