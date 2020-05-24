e04979cc99fff2768d804cde42ef5abce14cc08c
Romi Asad <rmasad@unc.edu>
2020-05-24 17:49:41 -0400

    Serious Commit message: Debugging parsing server text data

--- assets/js/parseTextFromServer.js
+++ assets/js/parseTextFromServer.js
@@ -88,7 +88,7 @@ function getSong() {
         console.log("Stage One");
 	    if (data.status == 200 && data.readyState == 4) {
             parseServerData(data.responseText);
-            textbox.value = serverObjects[0].songData;
+            console.log(serverObjects[0].songData);
         }
         console.log("Running");
     };
