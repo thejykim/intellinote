3a9858e914d211f795c6262b3d567f9602c93458
Romi Asad <rmasad@unc.edu>
2020-05-24 18:38:35 -0400

    testing

--- assets/js/parseTextFromServer.js
+++ assets/js/parseTextFromServer.js
@@ -94,7 +94,7 @@ function getSong() {
     data.open("POST", `assets/php/get-song.php?userID=${userID}&songID=${songID}`, true);
     data.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     data.send(); //parems
-    return data.responseText;
+    console.log("RETURN TEXT: " + data.responseText);
 }
 
 // Get songs from database
