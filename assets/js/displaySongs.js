1ab0401c1fffd3fe012f5607c92b4f53f8f4c5fb
Luke Duan <lduan11@live.unc.edu>
2020-05-28 22:32:27 -0400

    try pt 6 :D

--- assets/js/displaySongs.js
+++ assets/js/displaySongs.js
@@ -108,12 +108,14 @@ function displaySongs(userID) {
                 let songDiv = document.createElement('div');
                 songDiv.setAttribute("class", "columns");
                 songDiv.setAttribute("style", "padding-bottom: 0.5rem");
+                let songID = recentSongs[i].songID;
+                songID = songID.toString();
                 songDiv.innerHTML = `
                 <div class="column is-6 is-offset-3">
                     <div class="card" style="border-radius: 20px; border: 1px solid lightgray">
                         <div class="card-content">
                             <div class="content">
-                                <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href="#" class="has-text-dark">${recentSongs[i].title}</a></h5>
+                                <h5 class="title is-5 poppins" style="margin-bottom:0.5rem"><a href='songs.php?userID="${userID}"&songID=${songID}' class="has-text-dark">${recentSongs[i].title}</a></h5>
                                 <span class="tag gradient has-text-white poppins">Last updated: ${recentSongs[i].dateModified}</span>
                                 <span class="tag is-light poppins"><b>${numRows}</b></span>
                             </div>
