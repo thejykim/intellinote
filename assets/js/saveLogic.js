4504687feab72e60916d4d4f05b6bac3f80616f1
Romi Asad <rmasad@unc.edu>
2020-06-04 16:24:52 -0400

    Adding semicolon

--- assets/js/saveLogic.js
+++ assets/js/saveLogic.js
@@ -1,4 +1,4 @@
-let button = document.getElementByID("open-save-modal")
+let button = document.getElementById("open-save-modal");
 function saveLogic() {
     console.log("saveLogic running")
     button.innerHTML = "Testing";
