5a692a692c50728e735b48d9a0ebd83c961d01dd
Romi Asad <rmasad@unc.edu>
2020-06-04 16:35:44 -0400

    Testing prompt

--- assets/js/saveLogic.js
+++ assets/js/saveLogic.js
@@ -1,6 +1,19 @@
 let button = document.getElementById("open-save-modal");
 function saveLogic() {
-    console.log("saveLogic running")
-    button.innerHTML = "Testing";
 
+    // First-timer new user
+    if (oauthID == "Null") {
+        promptSignIn();
+    } else {
+        console.log("logged in");
+    }
+    // Logged in, about to create new song
+
+
+    //button.innerHTML = "Testing";
+
+}
+
+function promptSignIn() {
+    console.log("PROMPTING SIGN IN");
 }
