3aa711c956c8e3bcec99c05bf4cd57000cc91bf0
Luke Duan <lduan11@live.unc.edu>
2020-06-12 18:14:12 -0400

    fixed more displaySongs errors

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,10 +2,7 @@
 function onSignIn(googleUser) {
     if (!visitor) {
         var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         oauthID = profile.getId();
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
         email = profile.getEmail(); // This is null if the 'email' scope is not present.
         getUsername();
 
