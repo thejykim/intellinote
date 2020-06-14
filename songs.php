0da1f50eca326c53d6857690c6d5dbf2cf5b4edc
Joseph Kim <thejykim1@gmail.com>
2020-06-14 12:56:43 +0900

    Broken code from merge

--- songs.php
+++ songs.php
@@ -30,6 +30,9 @@
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         </script>
 
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
     </head>
 
     <body>
