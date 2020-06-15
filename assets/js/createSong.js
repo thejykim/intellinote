e794f1adb0f6b645d0a5f872a6b58882e619e0a0
Romi <rmasad@unc.edu>
2020-06-15 18:32:18 -0400

    Test

---
+++ assets/js/createSong.js
@@ -0,0 +1,45 @@
+/* Cleaned */
+
+function createSongIndex() {
+    // check required fields
+    if (titleField.value.length == 0) {
+        createDialog.innerHTML = `
+        <div class="notification is-warning">
+            Put in a title!
+        </div>
+        <br>
+        `;
+        return;
+    }
+
+    createNewSong();
+    createDialog.innerHTML = `
+    <a href='profile.php?username="${oauthUsername}"'>
+        <div class="notification is-success">
+            Song successfully added! Click this notification to visit your new profile.
+        </div>
+    </a>
+    <br>
+    `;
+}
+
+function editSongIndex(songID) {
+    // check required fields
+    if (titleField.value.length == 0) {
+        createDialog.innerHTML = `
+        <div class="notification is-warning">
+            Put in a title!
+        </div>
+        <br>
+        `;
+        return;
+    }
+
+    editSong(songID);
+    createDialog.innerHTML = `
+    <div class="notification is-success">
+        Song successfully saved!
+    </div>
+    <br>
+    `;
+}
