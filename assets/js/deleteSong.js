28e19b7e4b2229112b76ef859b6bdef6d45cdcc5
Romi <rmasad@unc.edu>
2020-06-17 17:51:28 -0400

    Testing button in popup

--- assets/js/deleteSong.js
+++ assets/js/deleteSong.js
@@ -2,6 +2,12 @@ function deletePopup() {
   createDialog.innerHTML = `
   <div class="notification is-danger">
     Are you sure you want to delete?
+    <button class = "button is-danger is-rounded"
+     onclick = "deleteSong()"> Delete Song </button>
   </div>
   <br>`;
+}
+
+function deleteSong() {
+  console.log("Deleting song");
 }
