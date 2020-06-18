f5667e1ec60fbf611954e19b3e2d02422e0bcb7f
Romi Asad <rmasad@unc.edu>
2020-06-18 17:02:13 -0400

    Testing functionality

--- assets/js/deleteSong.js
+++ assets/js/deleteSong.js
@@ -1,10 +1,11 @@
 function toggleDelete() {
     deleteContent.innerHTML = "Delete Song?";
     deleteButton.classList.add("is-light");
-    deleteButton.onclick = deleteSong;
+    deleteButton.onclick = callDelete;
 }
 
-function deleteSong() {
+function callDelete() {
+    deleteSong(globalSongID);
     deleteContent.innerHTML = "Song Deleted!";
     deleteButton.onclick = function() { return false; };
 }
