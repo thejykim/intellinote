f24878b5b8c36fc98d764b7de2e4def10b119b4f
Romi Asad <rmasad@unc.edu>
2020-06-24 23:50:08 -0400

    Is this it?

--- assets/js/deleteSong.js
+++ assets/js/deleteSong.js
@@ -29,12 +29,16 @@ function callProfileDelete() {
 }
 
 function toggleRecentDelete(songID) {
-    console.log("SONG ID IS: " + songID);
+    buttonToChange = document.getElementById(songID);
+    buttonToChange.innerHTML = "Confirm Delete?";
+    buttonToChange.classList.add("is-light");
+    songToDelete = songID.substr(1);
+    buttonToChange.onclick = callRecentDelete;
 }
 
 function callRecentDelete() {
-    document.getElementById(songToDelete).innerHTML = "Song Deleted!";
-    document.getElementById(songToDelete).setAttribute("disabled", "true");
+    buttonToChange.innerHTML = "Song Deleted!";
+    buttonToChange.setAttribute("disabled", "true");
     deleteSong(songToDelete);
     setTimeout(() => {}, 2000);
     window.location.href = "profile.php";
