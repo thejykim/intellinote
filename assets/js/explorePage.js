ce0e0772cb500a43dd606b46eafabf930839102a
Joseph Kim <thejykim1@gmail.com>
2020-08-27 19:20:40 -0400

    Rearranged tiles

--- assets/js/explorePage.js
+++ assets/js/explorePage.js
@@ -7,9 +7,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification tile-border">
                         <div>
                             <a href='songs.php?songID=${serverObjects[0].songID}'>
-                                <p class="title">${serverObjects[0].title}</p>
+                                <p class="title is-4">${serverObjects[0].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[0].username}"'>
                                     <strong>${serverObjects[0].username}</strong>
                                 </a>
@@ -19,9 +19,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification tile-border">
                         <div>
                             <a href='songs.php?songID=${serverObjects[1].songID}'>
-                                <p class="title">${serverObjects[1].title}</p>
+                                <p class="title is-4">${serverObjects[1].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[1].username}"'>
                                     <strong>${serverObjects[1].username}</strong>
                                 </a>
@@ -33,9 +33,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification is-info">
                         <div>
                             <a href='songs.php?songID=${serverObjects[2].songID}'>
-                                <p class="title">${serverObjects[2].title}</p>
+                                <p class="title is-4">${serverObjects[2].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[2].username}"'>
                                     <strong class="has-text-white">${serverObjects[2].username}</strong>
                                 </a>
@@ -47,9 +47,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification tile-border">
                         <div>
                             <a href='songs.php?songID=${serverObjects[3].songID}'>
-                                <p class="title">${serverObjects[3].title}</p>
+                                <p class="title is-4">${serverObjects[3].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[3].username}"'>
                                     <strong>${serverObjects[3].username}</strong>
                                 </a>
@@ -59,9 +59,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification tile-border">
                         <div>
                             <a href='songs.php?songID=${serverObjects[4].songID}'>
-                                <p class="title">${serverObjects[4].title}</p>
+                                <p class="title is-4">${serverObjects[4].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[4].username}"'>
                                     <strong>${serverObjects[4].username}</strong>
                                 </a>
@@ -71,27 +71,15 @@ function updateExplorePage() {
                 </div>
             </div>
             <div class="tile">
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child notification tile-border">
+                <div class="tile is-parent">
+                    <article class="tile is-child notification is-info">
                         <div>
                             <a href='songs.php?songID=${serverObjects[5].songID}'>
-                                <p class="title">${serverObjects[5].title}</p>
+                                <p class="title is-4">${serverObjects[5].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[5].username}"'>
                                     <strong>${serverObjects[5].username}</strong>
-                                </a>
-                            </p>
-                        </div>
-                    </article>
-                    <article class="tile is-child notification tile-border">
-                        <div>
-                            <a href='songs.php?songID=${serverObjects[6].songID}'>
-                                <p class="title">${serverObjects[6].title}</p>
-                            </a>
-                            <p class="subtitle karla">by 
-                                <a href='profile.php?username="${serverObjects[6].username}"'>
-                                    <strong>${serverObjects[6].username}</strong>
                                 </a>
                             </p>
                         </div>
@@ -100,20 +88,26 @@ function updateExplorePage() {
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification tile-border">
                         <div>
-                            <a href='songs.php?songID=${serverObjects[7].songID}'>
-                                <p class="title">${serverObjects[7].title}</p>
+                            <a href='songs.php?songID=${serverObjects[6].songID}'>
+                                <p class="title is-4">${serverObjects[6].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
-                                <a href='profile.php?username="${serverObjects[7].username}"'>
-                                    <strong>${serverObjects[7].username}</strong>
+                            <p class="subtitle is-6 karla">by 
+                                <a href='profile.php?username="${serverObjects[6].username}"'>
+                                    <strong>${serverObjects[6].username}</strong>
                                 </a>
                             </p>
                         </div>
                     </article>
                     <article class="tile is-child notification tile-border">
                         <div>
-                            <p class="title">See your own song here</p>
-                            <p class="subtitle karla">Create some music today</p>
+                            <a href='songs.php?songID=${serverObjects[7].songID}'>
+                                <p class="title is-4">${serverObjects[7].title}</p>
+                            </a>
+                            <p class="subtitle is-6 karla">by 
+                                <a href='profile.php?username="${serverObjects[7].username}"'>
+                                    <strong>${serverObjects[7].username}</strong>
+                                </a>
+                            </p>
                         </div>
                     </article>
                 </div>
@@ -121,9 +115,9 @@ function updateExplorePage() {
                     <article class="tile is-child notification is-info">
                         <div>
                             <a href='songs.php?songID=${serverObjects[8].songID}'>
-                                <p class="title">${serverObjects[8].title}</p>
+                                <p class="title is-4">${serverObjects[8].title}</p>
                             </a>
-                            <p class="subtitle karla">by 
+                            <p class="subtitle is-6 karla">by 
                                 <a href='profile.php?username="${serverObjects[8].username}"'>
                                     <strong class="has-text-white">${serverObjects[8].username}</strong>
                                 </a>
