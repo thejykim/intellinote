ad15b71cd4f6a85e0620a2784282a6f6bcd0e7ec
Joseph Kim <thejykim1@gmail.com>
2020-08-27 19:16:14 -0400

    Alignment changes within tiles

--- assets/js/explorePage.js
+++ assets/js/explorePage.js
@@ -5,110 +5,130 @@ function updateExplorePage() {
             <div class="tile">
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[0].songID}'>
-                            <p class="title">${serverObjects[0].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[0].username}"'>
-                                <strong>${serverObjects[0].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[0].songID}'>
+                                <p class="title">${serverObjects[0].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[0].username}"'>
+                                    <strong>${serverObjects[0].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[1].songID}'>
-                            <p class="title">${serverObjects[1].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[1].username}"'>
-                                <strong>${serverObjects[1].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[1].songID}'>
+                                <p class="title">${serverObjects[1].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[1].username}"'>
+                                    <strong>${serverObjects[1].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                 </div>
                 <div class="tile is-parent">
                     <article class="tile is-child notification is-info">
-                        <a href='songs.php?songID=${serverObjects[2].songID}'>
-                            <p class="title">${serverObjects[2].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[2].username}"'>
-                                <strong class="has-text-white">${serverObjects[2].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[2].songID}'>
+                                <p class="title">${serverObjects[2].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[2].username}"'>
+                                    <strong class="has-text-white">${serverObjects[2].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                 </div>
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[3].songID}'>
-                            <p class="title">${serverObjects[3].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[3].username}"'>
-                                <strong>${serverObjects[3].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[3].songID}'>
+                                <p class="title">${serverObjects[3].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[3].username}"'>
+                                    <strong>${serverObjects[3].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[4].songID}'>
-                            <p class="title">${serverObjects[4].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[4].username}"'>
-                                <strong>${serverObjects[4].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[4].songID}'>
+                                <p class="title">${serverObjects[4].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[4].username}"'>
+                                    <strong>${serverObjects[4].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                 </div>
             </div>
             <div class="tile">
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[5].songID}'>
-                            <p class="title">${serverObjects[5].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[5].username}"'>
-                                <strong>${serverObjects[5].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[5].songID}'>
+                                <p class="title">${serverObjects[5].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[5].username}"'>
+                                    <strong>${serverObjects[5].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[6].songID}'>
-                            <p class="title">${serverObjects[6].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[6].username}"'>
-                                <strong>${serverObjects[6].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[6].songID}'>
+                                <p class="title">${serverObjects[6].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[6].username}"'>
+                                    <strong>${serverObjects[6].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                 </div>
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification tile-border">
-                        <a href='songs.php?songID=${serverObjects[7].songID}'>
-                            <p class="title">${serverObjects[7].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[7].username}"'>
-                                <strong>${serverObjects[7].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[7].songID}'>
+                                <p class="title">${serverObjects[7].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[7].username}"'>
+                                    <strong>${serverObjects[7].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                     <article class="tile is-child notification tile-border">
-                        <p class="title">See your own song here</p>
-                        <p class="subtitle karla">Create some music today</p>
+                        <div>
+                            <p class="title">See your own song here</p>
+                            <p class="subtitle karla">Create some music today</p>
+                        </div>
                     </article>
                 </div>
                 <div class="tile is-parent">
                     <article class="tile is-child notification is-info">
-                        <a href='songs.php?songID=${serverObjects[8].songID}'>
-                            <p class="title">${serverObjects[8].title}</p>
-                        </a>
-                        <p class="subtitle karla">by 
-                            <a href='profile.php?username="${serverObjects[8].username}"'>
-                                <strong class="has-text-white">${serverObjects[8].username}</strong>
+                        <div>
+                            <a href='songs.php?songID=${serverObjects[8].songID}'>
+                                <p class="title">${serverObjects[8].title}</p>
                             </a>
-                        </p>
+                            <p class="subtitle karla">by 
+                                <a href='profile.php?username="${serverObjects[8].username}"'>
+                                    <strong class="has-text-white">${serverObjects[8].username}</strong>
+                                </a>
+                            </p>
+                        </div>
                     </article>
                 </div>
             </div>
