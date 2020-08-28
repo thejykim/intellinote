16c5f11b8490b140c6ce16fcf4cfe9ee43ea6874
Joseph Kim <thejykim1@gmail.com>
2020-08-28 10:38:06 -0400

    Fixed notes layering over sheet table

--- assets/js/noteLogic.js
+++ assets/js/noteLogic.js
@@ -12,9 +12,9 @@ function assignNote() {
 // onToggleNote()
 function parseNoteLen(noteLoc) {
     if(noteLoc[1] == 0 || noteLoc[1] == 12) {
-      noteIcon = '<img src = "assets/img/eighthNoteLedger.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+      noteIcon = '<img src = "assets/img/eighthNoteLedger.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
     } else {
-      noteIcon = '<img src = "assets/img/eighthNote.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+      noteIcon = '<img src = "assets/img/eighthNote.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
     }
 
     let lengths = ["eighthNote", "quarterNote", "halfNote", "wholeNote"];
@@ -40,25 +40,25 @@ function setEighth(noteLoc) {
     if (noteLoc == 0 || noteLoc == 12) {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/eighthNoteLedger.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNoteLedger.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/eighthNoteSharpLedger.png" style = "display: block; right: 15%; bottom: -130%; position: absolute; height: 2.55rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNoteSharpLedger.png" style = "display: block; right: 15%; bottom: -130%; position: absolute; height: 2.55rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/eighthNoteFlatLedger.png" style = "display: block; right: 15%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNoteFlatLedger.png" style = "display: block; right: 15%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     } else {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/eighthNote.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNote.png" style = "display: block; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/eighthNoteSharp.png" style = "display: block; right: 15%; bottom: -130%; position: absolute; height: 2.55rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNoteSharp.png" style = "display: block; right: 15%; bottom: -130%; position: absolute; height: 2.55rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/eighthNoteFlat.png" style = "display: block; right: 15%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/eighthNoteFlat.png" style = "display: block; right: 15%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     }
@@ -69,25 +69,25 @@ function setQuarter(noteLoc) {
     if (noteLoc == 0 || noteLoc == 12) {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/quarterNoteLedger.png" style = "display: block; right: 12%; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNoteLedger.png" style = "display: block; right: 12%; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/quarterNoteSharpLedger.png" style = "display: block; right: 18%; bottom: -120%; position: absolute; height: 2.6rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNoteSharpLedger.png" style = "display: block; right: 18%; bottom: -120%; position: absolute; height: 2.6rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/quarterNoteFlatLedger.png" style = "display: block; right: 18%; bottom: -95%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNoteFlatLedger.png" style = "display: block; right: 18%; bottom: -95%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     } else {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/quarterNote.png" style = "display: block; right: 12%; bottom: 0; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNote.png" style = "display: block; right: 12%; bottom: 0; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/quarterNoteSharp.png" style = "display: block; right: 18%; bottom: -120%; position: absolute; height: 2.6rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNoteSharp.png" style = "display: block; right: 18%; bottom: -120%; position: absolute; height: 2.6rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/quarterNoteFlat.png" style = "display: block; right: 18%; bottom: -95%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/quarterNoteFlat.png" style = "display: block; right: 18%; bottom: -95%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     }
@@ -98,25 +98,25 @@ function setHalf(noteLoc) {
     if (noteLoc == 0 || noteLoc == 12) {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/halfNoteLedger.png" style = "display: block; right: 12%; bottom: -10%; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNoteLedger.png" style = "display: block; right: 12%; bottom: -10%; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/halfNoteSharpLedger.png" style = "display: block; right: 25%; bottom: -140%; position: absolute; height: 2.55rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNoteSharpLedger.png" style = "display: block; right: 25%; bottom: -140%; position: absolute; height: 2.55rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/halfNoteFlatLedger.png" style = "display: block; right: 13%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNoteFlatLedger.png" style = "display: block; right: 13%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     } else {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/halfNote.png" style = "display: block; right: 12%; bottom: -10%; position: absolute; height: 2rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNote.png" style = "display: block; right: 12%; bottom: -10%; position: absolute; height: 2rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/halfNoteSharp.png" style = "display: block; right: 18%; bottom: -140%; position: absolute; height: 2.55rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNoteSharp.png" style = "display: block; right: 18%; bottom: -140%; position: absolute; height: 2.55rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/halfNoteFlat.png" style = "display: block; right: 18%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/halfNoteFlat.png" style = "display: block; right: 18%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     }
@@ -127,25 +127,25 @@ function setWhole(noteLoc) {
     if (noteLoc == 0 || noteLoc == 12) {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/wholeNoteLedger.png" style = "display: block; left: 16%; bottom: -60%; position: absolute; width: 1rem; height: 2.1rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNoteLedger.png" style = "display: block; left: 16%; bottom: -60%; position: absolute; width: 1rem; height: 2.1rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/wholeNoteSharpLedger.png" style = "display: block; right: 24%; bottom: -145%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNoteSharpLedger.png" style = "display: block; right: 24%; bottom: -145%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/wholeNoteFlatLedger.png" style = "display: block; right: 23%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNoteFlatLedger.png" style = "display: block; right: 23%; bottom: -110%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     } else {
         switch (accidental) {
             case "":
-                noteIcon = '<img src = "assets/img/wholeNote.png" style = "display: block; left: 16%; bottom: -15%; position: absolute; width: 0.8rem; height: 2.1rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNote.png" style = "display: block; left: 16%; bottom: -15%; position: absolute; width: 0.8rem; height: 2.1rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "#":
-                noteIcon = '<img src = "assets/img/wholeNoteSharp.png" style = "display: block; right: 24%; bottom: -135%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNoteSharp.png" style = "display: block; right: 24%; bottom: -135%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
             case "b":
-                noteIcon = '<img src = "assets/img/wholeNoteFlat.png" style = "display: block; right: 23%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle;" >';
+                noteIcon = '<img src = "assets/img/wholeNoteFlat.png" style = "display: block; right: 23%; bottom: -100%; position: absolute; height: 2.5rem; vertical-align: middle; z-index: -5" >';
                 break;
         }
     }
